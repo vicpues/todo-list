daf33b5f2c9852871603a4631fed27793f694180
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-08-28 00:52:15 +0200

    refactor: Remove type checking from Task class

--- src/models/todo-item.js
+++ src/models/todo-item.js
@@ -1,56 +1,20 @@
-export default class TodoItem {
+export default class Task {
     constructor({
-            title,
-            priority,
+            title = "",
+            priority = 1,
+            dueDate = new Date(0),
             description = "",
             project = "",
-            dueDate = new Date(0),
             icon = "",
             repeatsEvery = null,
     }) {
-        this._title = title;
-        this._priority = priority;
+        this.title = title;
+        this.priority = priority;
+        this.dueDate = dueDate;
         this.description = description;
         this.project = project;
-        this._dueDate = dueDate;
         this.icon = icon;
         this.repeatsEvery = repeatsEvery;
         this.isCompleted = false;
-    }
-
-    ZERO_DATE = new Date(0);
-    
-
-    get title() {return this._title;}
-    set title(newTitle) {
-        if (!newTitle || typeof newTitle !== "string") {
-            throw new TypeError("ToDo title must be a non-empty string");
-        } else {
-            this._title = newTitle;
-        }
-    }
-
-
-    get priority() {return this._priority;}
-    set priority(newPriority) {
-        if (!["high", "medium", "low"].includes(newPriority)) {
-            throw new TypeError('ToDo priority must be "high", "medium" or "low"');
-        } else {
-            this._priority = newPriority;
-        }
-    }
-
-    
-    get dueDate() {
-        return (this._dueDate === this.ZERO_DATE)
-            ? null
-            : this._dueDate
-    }
-    set dueDate(newDate) {
-        if (isNaN(new Date(newDate))) {
-            throw TypeError("Invalid date");
-        } else {
-            this._dueDate = newDate;
-        };
     }
 };
